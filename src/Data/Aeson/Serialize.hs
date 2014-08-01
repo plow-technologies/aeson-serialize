5b95836f7249f69d71d16a3dda73728b3feaebf5
Kevin Cotrone <kevincotrone@gmail.com>
2014-08-01 11:43:04 -0500

    Done? Testing needs to be done but the initial idea is done

--- src/Data/Aeson/Serialize.hs
+++ src/Data/Aeson/Serialize.hs
@@ -1,3 +1,11 @@
 module Data.Aeson.Serialize where
 
-import Data.Aeson.Serialize.Internal
+import           Data.Aeson
+import           Data.Serialize hiding (decode, encode)
+
+
+putToJSON :: (ToJSON a) => Putter a
+putToJSON = put . encode
+
+getToJSON :: (FromJSON a) => Get a
+getToJSON = get >>= (either fail return) . eitherDecode
