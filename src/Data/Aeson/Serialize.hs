ed7430c6f5550b37fd3925b2d81668a11b6b9a48
Kevin Cotrone <kevincotrone@gmail.com>
2014-08-01 15:13:14 -0500

    Fixing definitions

--- src/Data/Aeson/Serialize.hs
+++ src/Data/Aeson/Serialize.hs
@@ -3,9 +3,13 @@ module Data.Aeson.Serialize where
 import           Data.Aeson
 import           Data.Serialize hiding (decode, encode)
 
-
+-- | put function for all serialization of a json type
 putToJSON :: (ToJSON a) => Putter a
 putToJSON = put . encode
 
+-- | get function for all deserialization of a json type
 getFromJSON :: (FromJSON a) => Get a
 getFromJSON = get >>= (either fail return) . eitherDecode
+
+
+-- The intended use is to define an instance of Serialize where get and put are putToJSON and getFromJSON respectively
